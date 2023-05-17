1a700546a8b20e176370829b10544badbfcd4a31
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-05-18 01:27:17 +0530

    feat: add go_router to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:code_nexus/core/repository/auth_repository.dart';
 import 'package:code_nexus/core/routes/router.dart';
+import 'package:code_nexus/core/theme/themes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import 'core/bloc/auth_bloc.dart';
 
@@ -42,17 +44,29 @@ class CodeNexusView extends StatefulWidget {
 }
 
 class _CodeNexusViewState extends State<CodeNexusView> {
+  late final GoRouter _router;
+  @override
+  void initState() {
+    super.initState();
+    _router = AppRouter.router(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: AppTheme.darkColorScheme,
+      ),
       themeMode: ThemeMode.light,
       theme: ThemeData.from(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xffd2afff), // lilac color using material3
-        ),
+        colorScheme: AppTheme.lightColorScheme,
       ),
-      routerConfig: AppRouter.router(context),
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      routeInformationProvider: _router.routeInformationProvider,
     );
   }
 }
