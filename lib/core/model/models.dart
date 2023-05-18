5ba5df1178d80b64778d8c0c6ffdd023bb7ed864
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-05-19 02:06:23 +0530

    chore: add models barrel file

---
+++ lib/core/model/models.dart
@@ -0,0 +1,7 @@
+export 'branch_model.dart';
+export 'folder_details_model.dart';
+export 'github_user_model.dart';
+export 'user_model.dart';
+export 'repository_detail_model.dart';
+export 'repository_info_model.dart';
+export 'organization_model.dart';
