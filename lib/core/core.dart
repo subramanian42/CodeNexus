e452dbe91cda84f6781088dba474e236e0736976
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-05-19 02:02:18 +0530

    feat: add core barrel file

---
+++ lib/core/core.dart
@@ -0,0 +1,6 @@
+export 'github_client.dart';
+export 'widgets/widgets.dart';
+export 'utils/constants.dart';
+export 'bloc/auth_bloc.dart';
+export 'model/models.dart';
+export 'repository/repositories.dart';
