c8ecd0922322a5d8ccaf2d0de8462041a20d6b3f
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-05-19 12:18:04 +0530

    removed color from file list view

--- lib/presentation/file_explorer/view/widgets/file_list_view.dart
+++ lib/presentation/file_explorer/view/widgets/file_list_view.dart
@@ -10,12 +10,11 @@ class FileListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: const Color(0xFFF2F5F8),
         child: switch (folderDetails.tree?.isEmpty ?? true) {
-          true => const EmptyBody(),
-          false => FileExplorerBody(
-              folderDetails: folderDetails,
-            ),
-        });
+      true => const EmptyBody(),
+      false => FileExplorerBody(
+          folderDetails: folderDetails,
+        ),
+    });
   }
 }
